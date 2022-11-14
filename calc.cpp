edd4a15c20fe23c6baa2cc844f1db9fff70320ad
geethika0815 <geethika0815@gmail.com>
2022-11-14 16:00:31 +0530

    created a file for add/sub of two numbers

---
+++ calc.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Cal{
+        private:
+                int x,y;
+        public:
+                Cal(){
+                        x=0;
+                        y=0;
+                }
+                int add(int ,int);
+                int sub(int ,int);
+};
+int Cal::add(int x,int y){
+        return x+y;
+}
+int Cal::sub(int x, int y){
+        return x-y;
+}
+int main(){
+        Cal obj;
+        cout<<"add is " <<obj.add(4,6)<<endl;
+        cout<<"sub is "<<obj.sub(6,5)<<endl;
+        return 0;
+}
+
